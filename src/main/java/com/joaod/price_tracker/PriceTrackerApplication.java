915abf1d6f4c51acf187240549bb4e0cb2223f3f
João Dalri <105470028+Dalri05@users.noreply.github.com>
2025-05-18 22:48:18 -0300

    initial commit

---
+++ src/main/java/com/joaod/price_tracker/PriceTrackerApplication.java
@@ -0,0 +1,14 @@
+package com.joaod.price_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceTrackerApplication.class, args);
+	}
+
+}
